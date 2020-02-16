2f673b8dd486db6f19f1a09f5d9551f90015f43a
Quinten Ross <qjross76@gmail.com>
2020-02-16 14:43:18 -0500

    fixed the jquery

--- script.js
+++ script.js
@@ -3,5 +3,7 @@ $(document).ready(function() {
 $("#whatToDo").mouseenter(function(){
     $(this).attr("src", "demo-motion.gif");
 });
-    
+    $("#whatToDo").mouseleave(function(){
+    $(this).attr("src", "demo-still.png");
+});
 }); 
